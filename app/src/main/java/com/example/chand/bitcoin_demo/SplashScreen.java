633de7b59a361cf426d286c9b352091770c45205
chand <jubinwant@gmail.com>
2019-06-08 09:30:23 +0530

    1st msg for test

---
+++ app/src/main/java/com/example/chand/bitcoin_demo/SplashScreen.java
@@ -0,0 +1,34 @@
+package com.example.chand.bitcoin_demo;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+        LogoLauncher logoLauncher = new LogoLauncher();
+        logoLauncher.start();
+    }
+
+    private class LogoLauncher extends Thread {
+        @Override
+        public void run() {
+            try {
+                sleep(2500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+            startActivity(intent);
+            SplashScreen.this.finish();
+        }
+    }
+}
